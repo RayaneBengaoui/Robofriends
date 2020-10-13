bc9e1d7f81d22add9744ea018fccb6eeb983c30b
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-10-13 15:47:21 +0200

    Revert "Creating the store + provider"
    This reverts commit 0653461cf73768cfa7b2e6a6fc35f943db017d94.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,42 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 import Scroll from "../components/Scroll";
-import ErrorBoundry from "../components/ErrorBoundry";
+import ErrorBoundry from "../components/ErrorBoundry"
 
 function App() {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: [],
+  //     searchField: "",
+  //   };
+  // }
+
   const [robots, setRobots] = useState([]);
   const [searchField, setSearchfield] = useState("");
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => response.json())
+  //     .then((users) => this.setState({ robots: users }));
+  // }
+
+  useEffect(()=> {
+        fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => setRobots(users));
-  }, []);
+      .then((users) => setRobots( users ));
+  }, [])
 
   const onSearchChange = (event) => {
     setSearchfield(event.target.value);
   };
 
-  const filteredRobots = robots.filter((robot) => {
-    return robot.name.toLowerCase().includes(searchField.toLowerCase());
-  });
+   
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
 
-  // If equals 0
-  return !robots.length ? (
-    <h1>Loading</h1>
-  ) : (
-    <div className="tc">
-      <h1 className="f1">RoboFriends</h1>
-      <SearchBox searchChange={onSearchChange} />
-      <Scroll>
-        <ErrorBoundry>
-          <CardList robots={filteredRobots} />
-        </ErrorBoundry>
-      </Scroll>
-    </div>
-  );
+    // If equals 0
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
+  
 }
 
 export default App;
